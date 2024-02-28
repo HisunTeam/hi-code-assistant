558a675839e8c089ba1fa46e3287957e6bee3c20
吕海龙 <lv_hl@hisuntech.com>
2024-02-28 15:18:10 +0800

    fix: ChatGLM3-6B

--- src/main/java/com/hisun/codeassistant/llms/client/self/SelfModelEnum.java
+++ src/main/java/com/hisun/codeassistant/llms/client/self/SelfModelEnum.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum SelfModelEnum {
-    ChatGLM3_6B("chatglm3-6b", "openai/THUDM/chatglm3-6b", "ChatGLM3-6B (8k)", 8192),
+    ChatGLM3_6B("chatglm3-6b", "THUDM/chatglm3-6b", "ChatGLM3-6B (8k)", 8192),
     GPT_3_5_0125_16k("gpt-3.5-turbo-0125", "gpt-3.5-turbo-0125",  "GPT-3.5 Turbo (16k)", 16384),
     GPT_4_0125_128K("gpt-4-0125-preview", "gpt-4-0125-preview", "GPT-4 Turbo (128k)", 128000);
     // model name
